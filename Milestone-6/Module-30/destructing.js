518b041c0da860ff3628ec5e104e1237f61005cb
jahirul_islam <jisjis115@gmail.com>
2026-03-09 23:14:41 +0600

    ES6 (Destructuring)

---
+++ Milestone-6/Module-30/destructing.js
@@ -0,0 +1,9 @@
+// Object Destructuring
+const person = {
+    name : 'jahirul Islam',
+    age : 21,
+    profession : 'student',
+    country : 'Bangladesh'
+}
+const {name , age , profession, country} = person;
+console.log(name); // Output: jahirul Islam
