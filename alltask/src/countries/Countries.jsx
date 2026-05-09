9d340febd17c3e0503d85f9162e9a0f8adcd7653
jahirul_islam <jisjis115@gmail.com>
2026-05-09 13:11:02 +0600

    init commit

--- alltask/src/countries/Countries.jsx
+++ alltask/src/countries/Countries.jsx
@@ -1,30 +1,51 @@
 import { use, useState } from "react";
-import './Countries.css';
 import Country from "../country/Country";
 
-export default function Countries ({countriesPromise, variable}) {
-    const [visitedCountries , setVisitedCountries] = useState ([])
+export default function Countries({ countriesPromise, variable }) {
+    const [visitedCountries, setVisitedCountries] = useState([]);
+    const [visitedFalg , setVisitedFlag] = useState ([])
     const allData = use(countriesPromise);
     const country = allData.countries;
-    const handelVisitedCountries = (country) => {
-        const name = [...visitedCountries, country];
-        setVisitedCountries(name)
+    const handelVisitedCountries = (country, visited) => {
+        if (visited) {
+            let name = visitedCountries.find((c) => c.name.common === country.name.common);
+            if (!name) {
+                name = [...visitedCountries, country]
+                setVisitedCountries(name)
+            }
+        } else {
+            const reminig = visitedCountries.filter(
+                c => c.name.common !== country.name.common
+            )
+            setVisitedCountries (reminig)
+        }
+    }
+    const handleFlage = (flag)=> {
+        const allFlag = [...visitedFalg, flag];
+        setVisitedFlag(allFlag)
     }
     console.log(country[1]);
     return (
-        <div className="w-11/12  py-10 ">
-        <h1 className="text-center">All Country name is here </h1>
-        <ul className="text-center">Visited Country name {visitedCountries.map((name)=> <li> {name.name.common}</li>)} </ul>
-        <div className="flex items-center justify-center">
-        <div className="grid grid-cols-3 w-10/12 ">
-        {
-            country.map((allCountry , index) => <Country  key={index} 
-            country = { allCountry}
-            handelVisitedCountries = {handelVisitedCountries}
-            ></Country>)
-        }
-        </div>
-        </div>
+        <div className="w-11/12  py-10 mx-auto text-white ">
+            <h1 className="text-center">All Country name is here </h1>
+            
+            <h1 className="text-center"> Total Visited Country {visitedCountries.length}</h1>
+            <h1 className="text-center">Visited Country Flag :</h1>
+            <div className="flex flex-wrap justify-center items-center w-8/12 mx-auto">
+                {visitedFalg.map((flag , index)=> <img className="w-20 " src= {flag} key={index} alt="" />)}
+            </div>
+            <ul className="text-center">Visited Country name {visitedCountries.map((name, index) => <li key={index}> {name.name.common}</li>)} </ul>
+            <div className="flex items-center justify-center">
+                <div className="grid grid-cols-3 w-10/12 gap-10 ">
+                    {
+                        country.map((allCountry, index) => <Country key={index}
+                            country={allCountry}
+                            handelVisitedCountries={handelVisitedCountries}
+                            handleFlage = {handleFlage}
+                        ></Country>)
+                    }
+                </div>
+            </div>
         </div>
     )
 }
--- alltask/src/country/Country.jsx
+++ alltask/src/country/Country.jsx
@@ -1,17 +1,37 @@
 import { useState } from "react"
 
-export default function Country({ country, handelVisitedCountries }) {
+export default function Country({ country, handelVisitedCountries, handleFlage }) {
     const [visited, setVisited] = useState(false);
-    handelVisitedCountries(country)
-    // console.log(country)
-    return (
-        <div className="border border-green-400">
-            <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
-            <h1>{country.name.common}</h1>
-            <button className="btn cursor-pointer border rounded-lg bg-green-700 text-white" onClick={() => {
-                setVisited((pre) => !pre)
+    const [addFlag, setFlag] = useState(false);
 
-            }}> {visited ? "visited" : "Not Visited"}</button>
+    const hangdleAddFlag = ()=> {
+        setFlag (pre => !pre)
+        handleFlage(country.flags.flags.png)
+    }
+    // console.log(country)
+    const handelVisite = () => {
+        const newVisited = !visited;
+        setVisited((pre) => !pre)
+        handelVisitedCountries(country, newVisited);
+
+
+    }
+    return (
+        <div className={` py-4 px-5 ${visited ? "bg-green-400 transition  duration-300" : "border-2  border-green-500 transition duration-300 "}`}>
+            <img className="w-full rounded-lg" src={country.flags.flags.png} alt={country.flags.flags.alt} />
+            <h1>{country.name.common}</h1>
+            <p>Population {country.population.population}</p>
+            <p>
+                {(country.population.population) > 30000000 ? "Big Country " : "Small Country"}
+            </p>
+         <div className="flex justify-between flex-nowrap items-center">
+                        <button className="btn cursor-pointer border rounded-lg py-3 px-5 transition duration-300 bg-green-700 text-white" onClick={() => {
+                handelVisite()
+            }}> {visited ? "Visited" : "Not Visited"}</button>
+            <button className="btn cursor-pointer border rounded-lg py-3 px-5 transition duration-300 bg-green-700 text-white" onClick={() => {
+                hangdleAddFlag()
+            }}> Add Flag </button>
+         </div>
         </div>
     )
 }
